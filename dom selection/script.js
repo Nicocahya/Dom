be0fe916f368a8bd1a661582227836d36aef4fae
Nicocahya <nr016082@gmail.com>
2024-02-20 08:49:52 +0700

    dom selection

---
+++ dom selection/script.js
@@ -0,0 +1,22 @@
+//Dom selection
+//document.getElementById()
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'Nicko Cahya';
+
+//document.getElementByTagName()
+// -> HTMCollections
+const p = document.getElementsByTagName('p');
+
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+//document.getElementsByClassName()
+//-> HTMLCollections
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Ini diubah dari javascript';
